e26cc04c972e37bd925c3277bd9d3bb7cb1f37fe
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2018-03-22 02:22:51 +0100

    Added header title & styling update :lipstick:

--- App.js
+++ App.js
@@ -3,8 +3,11 @@ import { Text, View } from 'react-native';
 import { Header } from 'react-native-elements';
 
 const App = () => (
-  <View style={{ flex: 1 }}>
-    <Header />
+  <View style={{ flex: 1, backgroundColor: '#ddd' }}>
+    <Header
+      centerComponent={{ text: 'Youtube Search', style: { color: '#fff' } }}
+      outerContainerStyles={{ backgroundColor: '#E62117' }}
+    />
   </View>
 );
 
